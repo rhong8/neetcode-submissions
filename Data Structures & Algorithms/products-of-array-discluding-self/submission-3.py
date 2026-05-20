a36b79e0aca1f1a917037be7a841f3b32dfd290c
Ryan <92057113+rhong8@users.noreply.github.com>
2026-05-20 13:04:33 -0400

    Add: products-of-array-discluding-self - submission-3

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-3.py
@@ -0,0 +1,37 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        #solution without division
+        forwards: list[int] = [1] * len(nums)
+        forward_product = 1
+        backwards: list[int] = [1] * len(nums)
+        backwards_product = 1
+
+        for i in range(len(nums)):
+            forwards[i] = forward_product * nums[i]
+            forward_product = forward_product * nums[i]
+        
+        for k in range(len(nums)-1, -1, -1):
+            print(k)
+            backwards[k] = backwards_product * nums[k]
+            backwards_product = backwards[k]
+
+
+        print(f"forwards: {forwards}")
+        print(f"backwards: {backwards}")
+        print(forward_product)
+        print(backwards_product)
+
+        #putting together the solution now. the formula is f[i-1] * b[i+1], given that
+        # forward[i-1] represents the product of numbers before the current index, and b[i+1]
+        #represents the product of values afterwards. if i-1 or i+1 is out of bounds they are replaced by 1.
+        result: list[int] = [1] * len(nums)
+        for i in range(len(nums)):
+            if i == 0:
+                result[i] = backwards[1]
+            elif i == len(nums)-1:
+                result[i] = forwards[i-1]
+            else:
+                result[i] = forwards[i-1] * backwards[i+1]
+
+
+        return result

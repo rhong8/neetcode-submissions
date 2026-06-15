19114e45de4aa641bf2ccdfae7ff0a0070669a30
Ryan <92057113+rhong8@users.noreply.github.com>
2026-06-14 22:27:25 -0400

    Add: reorder-linked-list - submission-0

---
+++ Data Structures & Algorithms/reorder-linked-list/submission-0.py
@@ -0,0 +1,49 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        slow, fast = head, head
+        
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        
+        #find the midpoint
+            
+        prev = None
+        flipper = slow.next #the node after the midpoint
+
+        slow.next = None #sever the midpoint
+
+
+        flipperHead = None
+        while flipper != None:
+            if flipper.next == None:
+                flipperHead = flipper
+            temp = flipper.next
+            flipper.next = prev #rewrire
+            prev = flipper #move previous to flipper
+            flipper = temp #increment to the next
+            
+
+        #"thread the needle between the two lists"
+        curr1 = head #0
+        curr2 = flipperHead #2
+
+        while curr2: #neither of them is null
+            temp = curr1.next 
+            temp2 = curr2.next
+
+            curr1.next = curr2
+            
+
+            curr1 = temp #increment curr1
+            curr2.next = temp #BACK TO 1
+            curr2 = temp2 #increment curr2
+                #needs to connect back to curr1
+
+

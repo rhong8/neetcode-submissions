aa7988a6807e0542431321c5c0647a6ec90ed118
Ryan <92057113+rhong8@users.noreply.github.com>
2026-06-30 14:28:16 -0400

    Add: time-based-key-value-store - submission-6

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-6.py
@@ -0,0 +1,57 @@
+class TimeMap:
+
+    class Inner:
+        def __init__(self):
+            self.timestamps = []
+            self.val_map = defaultdict(str) #key: timestamp value: value
+
+
+    def __init__(self):
+        self.structures = defaultdict(TimeMap.Inner)#key: name value: innerstructure
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        self.structures[key].timestamps.append(timestamp) #timestamps are guranteed to increase
+        self.structures[key].val_map[timestamp] = value
+
+    def get(self, key: str, timestamp: int) -> str:
+        
+        
+        print(f"Attempting to find {timestamp}")
+        #binary search, converge on the closest value then walk the pointer backwards until it finds a value less than it
+        inner = self.structures[key]
+
+
+        l, r = 0, len(inner.timestamps) - 1
+        if r == -1: #then the structure doesn't exist
+            return '' 
+
+        mid = 0
+        found: int = -1 
+        while l <= r:
+
+            mid = (l + r) // 2
+            if inner.timestamps[mid] == timestamp: #found the exact timestamp in the nums array
+                found = inner.timestamps[mid]
+                break
+            elif inner.timestamps[mid] > timestamp: #split left
+                r = mid - 1
+            else:
+                l = mid + 1
+        
+        print(f"mid: {mid} timestamps: {inner.timestamps}")
+        print(f"full val_map: {inner.val_map.items()}")
+
+        if found == -1: #iterate the pointer back until it finds the appropiate value
+            while mid > 0 and inner.timestamps[mid] >= timestamp:
+                mid -= 1
+            #print(f"after iterating: {inner.val_map[inner.timestamps[mid]]}")
+            t = inner.timestamps[mid]
+            if t <= timestamp:
+                return inner.val_map[inner.timestamps[mid]]
+            else:
+                return '' #no timestamp before that was found
+        
+
+        print(f"found it: {inner.val_map[found]} at {found}")
+        return inner.val_map[found]
+        
